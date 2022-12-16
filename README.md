87485b1eaaf4702e221770e1591402df65572711
Raphael Pizzo <raphael.pizzo@pizzosoft.com.br>
2022-12-16 19:18:52 -0300

    integração com firebase
    integração com firebase

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# PizzoSoft
+Academic content about Next.js (version 13)
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
@@ -33,5 +35,4 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
 
-## PizzoSoft
-Academic content about Next.js (version 13)
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,33 +7,49 @@ import Tabela from '../components/Tabela'
 import Cliente from '../core/Cliente'
 import Botao from '../components/Botao'
 import Formulario from '../components/Formulario'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import ClienteRepositorio from '../core/ClienteRepositorio'
+import ColecaoCliente from '../backend/db/ColecaoCliente'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  const clientes = [
-    new Cliente('Ana1', 34, '1'),
-    new Cliente('Ana2', 33, '2'),
-    new Cliente('Ana3', 32, '3'),
-    new Cliente('Ana4', 31, '4'),
-    new Cliente('Ana5', 30, '5')
-  ]
+  const repo: ClienteRepositorio = new ColecaoCliente()
+
+  const [visivel, setVisivel] = useState<'tabela' | 'from'>('tabela')
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio)
+  const [clientes, setClientes] = useState<Cliente[]>([])
+
+  useEffect(obterTodos, [])
+
+  function obterTodos() {
+    repo.obterTodos().then(clientes => {
+      setClientes(clientes)
+      setVisivel('tabela')
+    })
+  }
 
   function clienteSelecionado(cliente: Cliente) {
     console.log(cliente)
+    setCliente(cliente)
+    setVisivel('from')
   }
 
-  function clienteExcluido(cliente: Cliente) {
-    console.log(cliente)
+  async function clienteExcluido(cliente: Cliente) {
+    await repo.excluir(cliente)
+    obterTodos()
   }
 
-  function salvarCliente(cliente: Cliente) {
-    console.log(cliente)
+  async function salvarCliente(cliente: Cliente) {
+    await repo.salvar(cliente)
+    obterTodos()
   }
 
-  const [visivel, setVisivel] = useState<'tabela' | 'from'>('tabela')
+  function novoCliente() {
+    setCliente(Cliente.vazio())
+    setVisivel('from')
+  }
 
   return (
     <div className={`
@@ -48,7 +64,7 @@ export default function Home() {
               <Botao
                 className='mb-4'
                 cor="green"
-                onClick={() => setVisivel("from")}
+                onClick={novoCliente}
               >
                 Novo Cliente
               </Botao>
@@ -60,7 +76,7 @@ export default function Home() {
             />
           </>
         ) : (
-          <Formulario cliente={clientes[3]}
+          <Formulario cliente={cliente}
             clienteMudou={salvarCliente}
             cancelado={() => setVisivel("tabela")} />
         )}
